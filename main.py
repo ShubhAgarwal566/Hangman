7d3b74463d120278899b3a155b92b7c87cc4e71f
ShubhAgarwal566 <shubhagarwal566@gmail.com>
2020-07-26 04:11:57 +0530

    Commented the Code

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import play
 
 pygame.init()
 
-width = 700
-height = 480	
+width = 700 # width of display window
+height = 480 # height of display window 
 win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Hangman")
+
 fnt = pygame.font.SysFont('comicsans', 30)
 FNT = pygame.font.SysFont('comicsans', 50)
 	
@@ -22,26 +24,26 @@ medium_words = ['abrupt', 'equip', 'galaxy', 'gossip', 'pixel', 'luxury', 'stren
 hard_words = ['recitation', 'valuation', 'testimony', 'awkward', 'microwave', 'rhythm', 'twelfth', 'xylophone']
 
 while(True):
-	win.fill((197, 227, 236))
-	win.blit(FNT.render("Welcome to Hangman", 0,(0,0,0)),(170, 100))  	
+	win.fill((197, 227, 236)) # fill with backgroud color
+	win.blit(FNT.render("Welcome to Hangman", 0,(0,0,0)),(170, 100)) # render and print text 	
 	options = ["EASY", "MEDIUM", "HARD"]
-	text = [fnt.render(options[i], 0, (0,0,0)) for i in range(3)]
+	text = [fnt.render(options[i], 0, (0,0,0)) for i in range(3)] # render text
 	x = [200, 400]
 	y = [200, 250, 300,350]
 	for i in range(4) :
 		pygame.draw.line(win, (0,0,0), (term_x[0], term_y[i]), (term_x[1], term_y[i]))
 	for i in range(2) :
 		pygame.draw.line(win, (0,0,0), (term_x[i], 200), (term_x[i], 350))
-
+	# above 2 for loops are used to draw the table for options
 	for i in range(3) :
-		win.blit(text[i], (315, y[i] + 15))  
-	pygame.display.update()
-	for event in pygame.event.get() :
-		#if quit detected
+		win.blit(text[i], (315, y[i] + 15))  # print the text
+	pygame.display.update() # update the screen
+	for event in pygame.event.get() : # wait for user input
+		#if clicked on cross or pressed <esc>
 		if(event.type == QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE) ):
 			pygame.quit()
 			sys.exit()         
-		#detecting the mouseclick and checking which button was selected (and breaking out from 2 loops)
+		#detecting the mouseclick and checking which button was selected
 		elif( event.type == MOUSEBUTTONDOWN ):
 			x, y = pygame.mouse.get_pos()
 			if( 250 < x < 450 and y > 200):
@@ -51,6 +53,7 @@ while(True):
 					play.Hangman(medium_words)
 				elif( y < 350 ):
 					play.Hangman(hard_words)
+		# detecting the level using keyboard
 		elif( event.type == KEYDOWN ):
 			if(event.key == K_e):
 				play.Hangman(easy_words)
--- play.py
+++ play.py
@@ -10,56 +10,56 @@ pygame.mixer.init()
 
 class Hangman:
 	def __init__(self, words):
-		self.width = 700
-		self.height = 480
+		self.width = 700 # width of game window	
+		self.height = 480 # height of game window
 		self.letters = {'a':False, 'b':False, 'c':False, 'd':False, 'e':False, 'f':False, 'g':False, 'h':False, 'i':False, 
 						'j':False, 'k':False, 'l':False, 'm':False, 'n':False, 'o':False, 'p':False, 'q':False, 'r':False, 
 						's':False, 't':False, 'u':False, 'v':False, 'w':False, 'x':False, 'y':False, 'z':False  }
+		#the dictionary above keeps track of all the letters which have been pressed by the user, False means not pressed
 		
-		self.backgroundColor = (197, 227, 236)
-		self.circleColor = (199, 66, 97)
+		self.backgroundColor = (197, 227, 236) #blue
+		self.circleColor = (199, 66, 97) #pink
 		self.fontColor = (0,0,0) #black
 
-		self.win = pygame.display.set_mode((self.width, self.height))
-		self.font = pygame.font.SysFont('comicsans', 30)
+		self.win = pygame.display.set_mode((self.width, self.height)) #creating the game window
+		self.font = pygame.font.SysFont('comicsans', 30) # saving this font style
 
-		self.hangman = 0
-		self.wordLetterSet = set()
-		self.numberOfLetters = 0
-		self.gameFlag = True
-		self.hintFlag = False
-		self.words = words
+		self.hangman = 0 # number of incorrect entries by user
+		self.wordLetterSet = set() # set of all the unique letters in the word
+		self.numberOfLetters = 0 # number of unique letters (used for HINT) 
+		self.gameFlag = True # flag which decides if the game is complete
+		self.hintFlag = False # flag which tells wether or not to show the HINT
+		self.words = words # self.words is the list of words, arg words is list of either easy,medium or hard words
 
-		self.font_render = []
+		self.font_render = [] # will hold rendered fonts to save time while running
 		for i in range(26):
-			self.font_render.append(self.font.render(chr(i+65), 1, self.fontColor))
-		self.font_render.append(self.font.render("_", 1, self.fontColor))
+			self.font_render.append(self.font.render(chr(i+65), 1, self.fontColor)) # adding all characters a-z
 
-		self.image_render = []
+		self.image_render = [] # will hold render images to save time while running
 		for i in range(1,8):
 			self.image_render.append(pygame.image.load("Images/{}.png".format(i)))
 
-		self.play()
+		self.play() # calling the play function
 
 	def initGame(self, word):
 		for i in range(len(word)):
-			pygame.draw.rect(self.win, (0,0,0), (50+i*30,440,15,2))
+			pygame.draw.rect(self.win, (0,0,0), (50+i*30,440,15,2)) # drawing the rectabgles which look like empty spaces ('__')
 
-		self.win.blit(self.image_render[0],(280,160))
+		self.win.blit(self.image_render[0],(280,160)) # putting the image of hangman with no errors
 		for i in word:
-			self.wordLetterSet.add(i)
-		self.numberOfLetters = len(self.wordLetterSet)
+			self.wordLetterSet.add(i) # adding all the letter to the set
+		self.numberOfLetters = len(self.wordLetterSet) # counting number of unique letters (for HINT)
 		
-	def renderScreen(self, text):
-		self.win.fill(self.backgroundColor)
-		fnt = pygame.font.SysFont("comicsans", 50)
-		if(text=="Win"):
+	def renderScreen(self, text): #funtion is called when the game is over
+		self.win.fill(self.backgroundColor) # removing all the elements
+		fnt = pygame.font.SysFont("comicsans", 50) 
+		if(text=="Win"): #if winner
 			self.win.blit(fnt.render("You "+text, True, (27, 48, 28)), (290,300))
 		else:
 			self.win.blit(fnt.render("You "+text, True, (161,0,0)), (260,300))
-		self.win.blit(self.image_render[6], (330, 220))
+		self.win.blit(self.image_render[6], (330, 220)) # image of the replay sign
 		pygame.display.update()
-		while True :
+		while True : #waiting for user input
 			for event in pygame.event.get() :
 				if( event.type == QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE) ):
 					pygame.quit()
@@ -71,54 +71,52 @@ class Hangman:
 				elif(event.type==KEYDOWN and event.key in [K_b, K_BACKSPACE, K_RETURN]):
 					return
 
-	def drawCircles(self):
+	def drawCircles(self): # used to draw circles with alphabets
 		for i in range(13):
-			pygame.draw.circle(self.win, self.circleColor, (i*50+50,40), 20)
-			if(self.letters[chr(i+97)] == False):
+			pygame.draw.circle(self.win, self.circleColor, (i*50+50,40), 20) #top 13 circles
+			if(self.letters[chr(i+97)] == False): # only puts aplhabets on letters which are not pressed yet 
 				self.win.blit(self.font_render[i], (i*50+43, 30))
 			
-			pygame.draw.circle(self.win, self.circleColor, (i*50+50,95), 20)
-			if(self.letters[chr(i+110)] == False):
+			pygame.draw.circle(self.win, self.circleColor, (i*50+50,95), 20) # bottom 13 circles
+			if(self.letters[chr(i+110)] == False): # only puts aplhabets on letters which are not pressed yet
 				self.win.blit(self.font_render[i+13], (i*50+43, 85))
 
-	def drawWord(self,word):
+	def drawWord(self,word): # used to draw the letters in the word
 		for i in range(len(word)):
-			if(self.letters[word[i]]==False):
-				pygame.draw.rect(self.win, (0,0,0), (50+i*30,440,15,2))
-			else:
+			if(self.letters[word[i]]==True): # draws letter which are correctly guessed
 				self.win.blit(self.font_render[ord(word[i])-97], (50+i*30,420))
 
 	def letterPressed(self, letter, word):
-		if(self.letters[letter] == True):
-			return True, True
-		self.letters[letter] = True
-		if(letter not in word):
-			self.hangman += 1
-			os.system('mpg123 -q --no-control '+"Audio/invalid.mp3")
-			pygame.draw.rect(self.win, self.backgroundColor, (280,160,200,250))
-			self.win.blit(self.image_render[self.hangman],(280,160))
-			if(self.hangman == 5):
-				return False, False				
-		else:
-			os.system('mpg123 -q --no-control '+"Audio/valid.mp3")
-			self.wordLetterSet.remove(letter)
-			if(len(self.wordLetterSet)==0):
-				return False, True
-		return True, True
+		if(self.letters[letter] == True): # if letter already pressed earlier
+			return True, True # return type is gameFlag, Result
+		self.letters[letter] = True # if not pressed earlier then mark it as pressed now
+		if(letter not in word): # if wrong letter
+			self.hangman += 1 #mark as error
+			os.system('mpg123 -q --no-control '+"Audio/invalid.mp3") # play inavalid sound
+			pygame.draw.rect(self.win, self.backgroundColor, (280,160,200,250)) # cover the old hangman photo
+			self.win.blit(self.image_render[self.hangman],(280,160)) # render new hangman photo
+			if(self.hangman == 5): # if 5 errors recorded
+				return False, False	# return type gameFlag, Result
+		else: # correct letter
+			os.system('mpg123 -q --no-control '+"Audio/valid.mp3") #play valid sound
+			self.wordLetterSet.remove(letter) # remove letter from remaining letters
+			if(len(self.wordLetterSet)==0): # if no letters remaining (all letters guessed correctly)
+				return False, True # return type gameFlag, Result
+		return True, True # for all other conditions, gameFlag, Result
 
 	def play(self):
-		self.win.fill(self.backgroundColor)
-		word = random.choice(self.words)
-		print(word)
-		self.initGame(word)
+		self.win.fill(self.backgroundColor) #color the background
+		word = random.choice(self.words) # choose a word at random
+		print("The word is : " + str(word)) # print on the console
+		self.initGame(word) 
+		pygame.display.set_caption("Hangman")
 		while(self.gameFlag):
-			pygame.display.set_caption("Hangman")
-			for event in pygame.event.get():
-					#quit game if user clicks the cross
+			for event in pygame.event.get(): # wait for user input
+					#quit game if user clicks the cross or pressed <esc>
 					if(event.type == QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE)):
 						pygame.quit()
 						sys.exit()
-					elif(event.type == KEYDOWN):
+					elif(event.type == KEYDOWN): 
 						if(event.key == K_a):
 							self.gameFlag,res = self.letterPressed('a', word)
 						elif(event.key == K_b):
@@ -174,19 +172,19 @@ class Hangman:
 						elif(event.key == K_RETURN):
 							self.hintFlag = not self.hintFlag
 						
-			if(self.hintFlag==True):
+			if(self.hintFlag==True): # show hint
 				self.win.blit(pygame.font.SysFont('Arial', 20).render("{} Unique Letters".format(self.numberOfLetters), True, (0,0,0)), (500,420))
-			else:
+			else: # cover with rectangle
 				pygame.draw.rect(self.win, self.backgroundColor, (500,420,200,250))
 			
 			self.drawCircles()
 			self.drawWord(word)
-			pygame.display.update()
-			if(self.gameFlag == False):
-				if(res == True):
+			pygame.display.update() # update the display screen
+			if(self.gameFlag == False): # if game is finished
+				if(res == True): # if player won
 					os.system('mpg123 -q --no-control '+"Audio/win.mp3")		
 					self.renderScreen("Win")
-				else:
+				else: # if player lost
 					os.system('mpg123 -q --no-control '+"Audio/loose.mp3")		
 					self.renderScreen("Loose")
 
